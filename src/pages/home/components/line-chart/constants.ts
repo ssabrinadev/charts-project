2c258deae94503b12d691482ec1011e361c7ae1d
ssabrinadev <babajanovasabrina4@gmail.com>
2024-12-08 22:23:07 +0500

    feat(charts): added recharts lib

---
+++ src/pages/home/components/line-chart/constants.ts
@@ -0,0 +1,32 @@
+export const data = [
+  {
+    web: 0,
+    telegram: 0,
+    youtube: 2,
+    name: 0,
+  },
+  {
+    web: 1.5,
+    telegram: 1,
+    youtube: 2.5,
+    name: 20,
+  },
+  {
+    web: 1.2,
+    telegram: 1.3,
+    youtube: 0.5,
+    name: 30,
+  },
+  {
+    web: 4,
+    telegram: 1.8,
+    youtube: 3.2,
+    name: 40,
+  },
+  {
+    web: 3.3,
+    telegram: 0.8,
+    youtube: 4.2,
+    name: 50,
+  },
+];
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,12 @@
-import { CustomDropdown } from '@ui';
+import { LineChartSection, PieChartSection } from './components';
 
 export const HomePage = () => {
   return (
     <main className="container">
-      <p>asd</p>
-      <CustomDropdown />
+      <div className="grid sm:grid-cols-2 gap-5">
+        <LineChartSection />
+        <PieChartSection />
+      </div>
     </main>
   );
 };
